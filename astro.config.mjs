b303bb88bf7909e6a5c51a21ee4458e4b5f877db
Ismael Barrera García <106593307+TerryIsmael@users.noreply.github.com>
2024-02-25 23:27:56 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
   site: 'http://www.cohabify.github.io/'
 });
