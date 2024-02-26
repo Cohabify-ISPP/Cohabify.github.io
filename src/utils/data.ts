28ae8c5bc01e7d92a79b77123f0d51538c765580
olemornav <olemornav@alum.us.es>
2024-02-26 13:48:12 +0100

    fix: Cambios visuales y en descripciones

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,25 +1,25 @@
 const services = [
     {
-        title: "Búsqueda pasiva de compañero de piso",
-        description: "Podrás anunciarte y buscar compañero de piso de forma pasiva, es decir, recibirás peticiones de personas que quieran compartir piso contigo.",
+        title: "Anúnciate como compañero",
+        description: "Publica tu perfil para que otros usuarios puedan contactarte.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
-        title: "Búsqueda activa de compañero de piso",
-        description: "Podrás buscar a tu compañero sin tener que esperar a que te contacten.",
+        title: "Encuentra a tu compañero",
+        description: "Busca y contacta con el compañero que se ajuste más a tus necesidades.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Búsqueda de piso",
-        description: "Podrás buscar habitaciones o pisos completos para compartir.",
+        description: "Encuentra habitaciones o pisos completos para compartir.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
         title: "Publicar piso",
-        description: "Como propietario podrás publicar tu piso para compartirlo con otras personas.",
+        description: "Anuncia tu piso para compartirlo con otras personas si eres propietario o anúncialo como usuario si necesitas compañeros.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
